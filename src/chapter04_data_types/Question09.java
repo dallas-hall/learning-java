428af12a451f6c0551d5b25ab30c09bd17cc13c3
Dallas Hall <mr.dallas.hall@gmail.com>
2018-03-10 20:51:27 +1100

    Fixed the unicode here, it was actually ASCII before and not unicode. Now it works properly.

--- src/chapter04_data_types/Question09.java
+++ src/chapter04_data_types/Question09.java
@@ -5,26 +5,23 @@ import java.util.Scanner;
 /**
  * Created by blindcant on 24/04/17.
  */
-public class Question09
-{
+public class Question09 {
 	//INSTANCE VARIABLES
 	private Scanner keyboard = new Scanner(System.in);
 	
 	//MAIN METHOD
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		Question09 run1 = new Question09();
 	}
 	
 	//CONSTRUCTOR(S)
-	Question09()
-	{
+	Question09() {
 		System.out.println("Find Unicode number.");
 		System.out.println("Enter a character:");
 		//convert input from String to Char
 		char userInput = keyboard.nextLine().charAt(0);
 		//display Unicode number
-		System.out.println("The Unicode for the character " + userInput + " is: " + (int)userInput);
+		System.out.println("The Unicode for the character " + userInput + " is 0x" + Integer.toHexString(userInput));
 	}
 	
 	//METHODS
