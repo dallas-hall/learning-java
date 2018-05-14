d044b2cf4ce8455573d57fedbfbf13b78c2f640a
Dallas Hall <dallas.hall@example.com>
2018-05-14 20:18:11 +1000

    Updated the class comment and code stylings.

--- src/chapter11_inheritance/Question02/Student.java
+++ src/chapter11_inheritance/Question02/Student.java
@@ -1,32 +1,40 @@
 package chapter11_inheritance.Question02;
 
 /**
- * Created by blindcant on 3/05/17.
+ * <h1>Student</h1>
+ * <p>
+ * This program is my implementation of a student using inheritance and interfaces.
+ * </p>
+ * <p>
+ * tags:	inheritance; extends; super class constructor; over riding;
+ * </p>
+ *
+ * @author blindcant
+ * @version 0.1 - 2017-05-03
  */
 public class Student extends Person
 {
-	//INSTANCE VARIABLES
+	//@@@ INSTANCE VARIABLES @@@
 	private final String classStatus;
 	
-	//CONSTRUCTOR(S)
+	//@@@ CONSTRUCTOR(S) @@@
 	public Student(String name, String streetAddress, String emailAddress, String phoneNumber, String classStatus)
 	{
 		super(name, streetAddress, emailAddress, phoneNumber);
 		this.classStatus = classStatus;
 	}
 	
-	//METHODS
+	//@@@ METHODS @@@
+	//### GETTERS ###
 	public String getClassStatus()
 	{
 		return classStatus;
 	}
 	
-	//HELPERS
-	
+	//### HELPERS ###
 	@Override
 	public String toString()
 	{
-		return super.toString() + String.format("Student status:\t\t%s\n"
-				,getClassStatus());
+		return super.toString() + String.format("Student status:\t\t%s\n", getClassStatus());
 	}
 }
