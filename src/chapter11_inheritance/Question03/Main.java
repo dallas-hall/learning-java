d23e5291a6ba6044cd4b633e5d6a5939cc39552a
Dallas Hall <mr.dallas.hall@gmail.com>
2018-05-14 20:23:21 +1000

    Update class comments and code stylz.

--- src/chapter11_inheritance/Question03/Main.java
+++ src/chapter11_inheritance/Question03/Main.java
@@ -3,40 +3,49 @@ package chapter11_inheritance.Question03;
 import chapter09_objects_and_classes.Question07;
 
 /**
- * Created by blindcant on 4/05/17.
+ * <h1>Account Driver</h1>
+ * <p>
+ * This is the driver class for Account, showcasing inheritance and polymorphism.
+ * </p>
+ * <p>
+ * tags:	inheritance; extends; super class constructor; packages;
+ * </p>
+ *
+ * @author blindcant
+ * @version 0.1 - 2017-05-04
  */
-public class TestQ3
+public class Main
 {
-	//INSTANCE VARIABLES
+	//@@@ INSTANCE VARIABLES @@@
 	private static Question07[] accountArray = new Question07[10];
 	private static int arrayCount = 0;
 	
-	//MAIN METHOD
+	//@@@ MAIN METHOD @@@
 	public static void main(String[] args)
 	{
 		accountArray[arrayCount++] = new Question07("Donny Darko", 5000, 3);
-		accountArray[arrayCount++] = new SavingsAccount("Donny Darko", 20000, 6, 1000);
+		accountArray[arrayCount++] = new SavingsAccount("Sonny Marko", 20000, 6, 1000);
 		
 		printArray();
 		
+		// Should pass
 		accountArray[1].withdraw(18000);
 		System.out.println(accountArray[1].toString());
 		
+		// Should fail due to minimum balance
 		accountArray[1].withdraw(1800);
 		System.out.println(accountArray[1].toString());
 		
+		// Should calculate
 		accountArray[1].getMonthlyInterest();
 		System.out.println(accountArray[1].toString());
 	}
 	
-	//CONSTRUCTOR(S)
-	
-	
-	//METHODS
+	//@@@ METHODS @@@
+	//### HELPERS ###
 	public static void printArray()
 	{
-		for (int i = 0; i < arrayCount; i++)
-		{
+		for (int i = 0; i < arrayCount; i++) {
 			System.out.println(accountArray[i].toString());
 		}
 	}
