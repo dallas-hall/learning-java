cf66e73156e36b3f3d7d54bc6cf9695940a9943d
Dallas Hall <mr.dallas.hall@gmail.com>
2018-05-17 20:12:04 +1000

    Added more functionality to this class. It does min, max, avg, sum. It will also sort ascending, reverse, remove duplicates, union join, and shuffle. They basically all use Collections methods to do this.

---
+++ src/chapter11_inheritance/Question04_07_11_to_14/Main.java
@@ -0,0 +1,47 @@
+package chapter11_inheritance.Question04_07_11_to_14;
+
+import java.util.List;
+
+/**
+ * <h1>ArrayList Values Main</h1>
+ * <p>
+ * This is the driver program for the ArrayList Values class.
+ * </p>
+ * <p>
+ * tags:
+ * </p>
+ *
+ * @author blindcant
+ * @version 0.0.1 - 2018-05-15
+ */
+public class Main
+{
+	//@@@ MAIN METHOD @@@
+	public static void main(String[] args)
+	{
+		System.out.println("[INFORMATION] Test 1.");
+		ArrayListManipulation runtime = new ArrayListManipulation();
+		runtime.populateList(20);
+		runtime.printList("The original list is: ");
+		System.out.println("The sum total of the list is: " + runtime.sumList());
+		System.out.println("The minimum value is: " + runtime.getMin());
+		System.out.println("The average value is: " + runtime.getAverage());
+		System.out.println("The maximum value is: " + runtime.getMax());
+		runtime.sortList();
+		runtime.printList("The sorted list is: ");
+		runtime.shuffleList();
+		runtime.printList("The shuffled list is: ");
+		runtime.reverseList();
+		runtime.printList("The reversed list is: ");
+		runtime.removeDuplicates();
+		runtime.printList("The distinct list is: ");
+		
+		System.out.println("\n[INFORMATION] Test 2.");
+		ArrayListManipulation runtime2 = new ArrayListManipulation();
+		runtime2.populateList();
+		runtime2.printList("The original list is: ");
+		runtime2.setList(runtime2.unionLists(runtime2.getList(), runtime.getList()));
+		runtime2.printList("The list after UNION is : ");
+		
+	}
+}
