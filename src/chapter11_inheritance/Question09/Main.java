ec9a560441af4205ee9a93876761215f5dd6271a
Dallas Hall <mr.dallas.hall@gmail.com>
2018-05-17 20:28:11 +1000

    Binary Matrix constructor done, makes a List that is storing ArrayLists. These ArrayLists will hold a single row of the matrix. The rows in the matrix can be populated with 0 or 1 using pseudo random numbers. The print matrix method is also available.

---
+++ src/chapter11_inheritance/Question09/Main.java
@@ -0,0 +1,24 @@
+package chapter11_inheritance.Question09;
+
+/**
+ * <h1>PROGRAM NAME GOES HERE</h1>
+ * <p>
+ * This program
+ * </p>
+ * <p>
+ * tags:	<insert concept tags here for training code only>
+ * </p>
+ *
+ * @author blindcant
+ * @version 0.0.1 - 2018-05-17
+ */
+public class Main
+{
+	//@@@ MAIN METHOD @@@
+	public static void main(String[] args)
+	{
+		BinaryMatrix runtime = new BinaryMatrix(4);
+		runtime.populateMatrix();
+		runtime.printBinaryMatrix("Original binary matrix is: ");
+	}
+}
