0e9deff1bac4f4f64e714b6c4269370b5f08abd2
Dallas Hall <dallas.hall@example.com>
2018-05-25 19:14:59 +1000

    File IO working.

--- src/chapter12_exceptions_and_text_io/Question12/MoveSourceCodeCurlyBrace.java
+++ src/chapter12_exceptions_and_text_io/Question12/MoveSourceCodeCurlyBrace.java
@@ -1,5 +1,11 @@
 package chapter12_exceptions_and_text_io.Question12;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,8 +18,8 @@ import java.util.regex.Pattern;
  * <p>
  * tags:	Pattern; Matcher; MULTILINE;
  * </p>
- *
- * Much help provided from here https://www.regular-expressions.info/java.html
+ * <p>
+ * Much help provided from here https://www.regular-expressions.info/java.html & https://regex101.com/
  *
  * @author blindcant
  * @version 0.0.1 - 2018-05-23
@@ -21,23 +27,31 @@ import java.util.regex.Pattern;
 public class MoveSourceCodeCurlyBrace
 {
 	//@@@ CLASS VARIABLES @@@
-	public static final String sameLine = "public class SameLine {\n\tpublic SameLine() {\n\t\tif(true) {\n\t\t\tSystem.out.println(\"Hello World\");\n\t\t}\n\t}\n}";
-	public static final String newLine = "public class NewLine\n{\n\tpublic NewLine()\n\t{\n\t\tif(true)\n\t\t{\n\t\t\tSystem.out.println(\"Hello World\");\n\t\t}\n\t}\n}";
+	public static final String sameLineString = "public class SameLine {\n\tpublic SameLine() {\n\t\tif(true) {\n\t\t\tSystem.out.println(\"Hello World\");\n\t\t}\n\t}\n}";
+	public static final String newLineString = "public class NewLine\n{\n\tpublic NewLine()\n\t{\n\t\tif(true)\n\t\t{\n\t\t\tSystem.out.println(\"Hello World\");\n\t\t}\n\t}\n}";
+	public static final String sameLineFile = "/media/veracrypt1/java-random-files-for-io/sameLine.java";
+	public static final String newLineFile = "/media/veracrypt1/java-random-files-for-io/newLine.java";
+	
 	
 	//@@@ INSTANCE VARIABLES @@@
-	Scanner scanner;
+	private List<Path> paths;
+	private BufferedReader bufferedReader;
+	private PrintWriter printWriter;
 	
 	//@@@ MAIN METHOD @@@
 	public static void main(String args[])
 	{
 		MoveSourceCodeCurlyBrace runtime = new MoveSourceCodeCurlyBrace();
-		runtime.testString();
+		//runtime.testString();
+		runtime.createFiles();
+		runtime.populateFiles();
+		runtime.readFilesAndPrint();
 	}
 	
 	//@@@ CONSTRUCTOR(S) @@@
 	public MoveSourceCodeCurlyBrace()
 	{
-		scanner = new Scanner(System.in);
+		paths = new ArrayList<>();
 	}
 	
 	//@@@ METHODS @@@
@@ -68,10 +82,84 @@ public class MoveSourceCodeCurlyBrace
 	public void testString()
 	{
 		System.out.println("[INFORMATION] Printing new line brace and then switching it to same line brace.");
-		System.out.println(newLine);
-		System.out.println(stringSwitchToSameLine(newLine));
+		System.out.println(newLineString);
+		System.out.println(stringSwitchToSameLine(newLineString));
 		System.out.println("\n[INFORMATION] Printing same line brace and then switching it to new line brace.");
-		System.out.println(sameLine);
-		System.out.println(stringSwitchToNewline(sameLine));
+		System.out.println(sameLineString);
+		System.out.println(stringSwitchToNewline(sameLineString));
+	}
+	
+	public void createFiles()
+	{
+		System.out.println("[INFORMATION] Creating files using Path & Files.");
+		Path sameLinePath = Paths.get(sameLineFile);
+		Path newLinePath = Paths.get(newLineFile);
+		paths.add(sameLinePath);
+		paths.add(newLinePath);
+		
+		for (Path currentPath : paths) {
+			//https://docs.oracle.com/javase/tutorial/essential/io/check.html
+			if (Files.exists(currentPath)) {
+				// Need -1 here to remove the filename
+				System.out.println("The file " + currentPath.getFileName() + " already exists at /" + currentPath.subpath(0, currentPath.getNameCount() - 1));
+			}
+			else {
+				System.out.println("The file " + currentPath.getFileName() + " will be created at /" + currentPath.subpath(0, currentPath.getNameCount() - 1));
+				try {
+					//https://docs.oracle.com/javase/tutorial/essential/io/file.html#creating
+					Files.createFile(currentPath);
+				}
+				catch (IOException e) {
+					System.out.println("[ERROR] Unable to create file.\n");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public void populateFiles()
+	{
+		System.out.println("[INFORMATION] Adding source code to the files.");
+		for (Path currentPath : paths) {
+			System.out.println("Source code added to " + currentPath.toString());
+			try {
+				printWriter = new PrintWriter(new FileWriter(currentPath.toFile()));
+				if (currentPath.getFileName().toString().equals("sameLine.java")) {
+					printWriter.print(sameLineString);
+				}
+				else {
+					printWriter.print(newLineString);
+				}
+			}
+			catch (IOException e) {
+				System.out.println("[ERROR] Unable to write to file.\n");
+				e.printStackTrace();
+			}
+			finally {
+				printWriter.close();
+			}
+		}
+	}
+	
+	public void readFilesAndPrint()
+	{
+		System.out.println("[INFORMATION] Reading source code from the files.");
+		for (Path currentPath : paths) {
+			System.out.println("Source code read from " + currentPath.toString());
+			try {
+				bufferedReader = bufferedReader = new BufferedReader(new FileReader(currentPath.toFile()));
+				// store the current line here
+				String currentLine;
+				// Store the current line as a String into currentLine.  The readLine() method returns null when the end of line is reached.
+				while ((currentLine = bufferedReader.readLine()) != null)
+				{
+					System.out.println(currentLine);
+				}
+			}
+			catch (IOException e) {
+				System.out.println("[ERROR] Unable to write to file.\n");
+				e.printStackTrace();
+			}
+		}
 	}
 }
