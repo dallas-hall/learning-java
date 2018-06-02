abd7c8d07ff29cbcffd366ecd1cabd193d5d73f4
Dallas Hall <dallas.hall@example.com>
2018-06-02 19:00:15 +1000

    Added the ability to count UPPERCASE and lowercase characters.

--- src/chapter12_exceptions_and_text_io/Question19and30/WebsiteFileReading.java
+++ src/chapter12_exceptions_and_text_io/Question19and30/WebsiteFileReading.java
@@ -1,6 +1,8 @@
-package chapter12_exceptions_and_text_io.Question19;
+package chapter12_exceptions_and_text_io.Question19and30;
 
 import java.net.URL;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -8,7 +10,7 @@ import java.util.regex.Pattern;
 /**
  * <h1>Website Reader & Counter</h1>
  * <p>
- * This program connects to a website and opens a file. It will then read in the file line by line, and count its characters, words, and lines.
+ * This program connects to a website and opens a file. It will then read in the file line by line, and count its characters, words, and lines. It now counts individual letters as well.
  * </p>
  * <p>
  * tags:	URL; Scanner(url.openStream()); Pattern; Matcher; regex;
@@ -29,6 +31,7 @@ public class WebsiteFileReading
 	private int lines;
 	private int words;
 	private int characters;
+	private Map<String, Integer> letterCount;
 	
 	//@@@ MAIN METHOD @@@
 	public static void main(String[] args)
@@ -36,17 +39,21 @@ public class WebsiteFileReading
 		WebsiteFileReading runtime = new WebsiteFileReading();
 		runtime.getWebsiteData(WEBSITE_URL);
 		runtime.printResults(WEBSITE_URL);
+		runtime.printLetterCountMap();
 	}
 	
 	//@@@ CONSTRUCTOR(S) @@@
 	public WebsiteFileReading(String websiteURL)
 	{
 		this.websiteURL = websiteURL;
+		letterCount = new LinkedHashMap<>();
+		populateMap();
 	}
 	
 	public WebsiteFileReading()
 	{
-	
+		letterCount = new LinkedHashMap<>();
+		populateMap();
 	}
 	
 	//@@@ METHODS @@@
@@ -62,6 +69,7 @@ public class WebsiteFileReading
 				lines++;
 				characters += currentLine.length();
 				words += countWords(currentLine);
+				countCharacters(currentLine);
 			}
 		}
 		catch (Exception e) {
@@ -70,6 +78,19 @@ public class WebsiteFileReading
 		}
 	}
 	//### SETTERS ###
+	private void populateMap()
+	{
+		int uppercaseCodePoint = 65;
+		int lowercaseCodePoint = 97;
+		
+		for (int i = uppercaseCodePoint; i < uppercaseCodePoint + 26; i++) {
+			letterCount.put(String.valueOf((char) i), 0);
+		}
+		
+		for (int i = lowercaseCodePoint; i < lowercaseCodePoint + 26; i++) {
+			letterCount.put(String.valueOf((char) i), 0);
+		}
+	}
 	
 	//### HELPERS ###
 	private long countWords(String currentLine)
@@ -85,6 +106,33 @@ public class WebsiteFileReading
 		return matchCount;
 	}
 	
+	private void countCharacters(String currentLine)
+	{
+		int matchCount = 0;
+		int uppercaseCodePoint = 65;
+		int lowercaseCodePoint = 97;
+		
+		for (int i = uppercaseCodePoint; i < uppercaseCodePoint + 26; i++) {
+			Pattern pattern = Pattern.compile(String.valueOf((char) i));
+			Matcher matcher = pattern.matcher(currentLine);
+			// https://stackoverflow.com/a/7378472
+			while (matcher.find()) {
+				matchCount++;
+			}
+			letterCount.put(String.valueOf((char) i), matchCount + letterCount.get(String.valueOf((char) i)));
+		}
+		
+		for (int i = lowercaseCodePoint; i < lowercaseCodePoint + 26; i++) {
+			Pattern pattern = Pattern.compile(String.valueOf((char) i));
+			Matcher matcher = pattern.matcher(currentLine);
+			// https://stackoverflow.com/a/7378472
+			while (matcher.find()) {
+				matchCount++;
+			}
+			letterCount.put(String.valueOf((char) i), matchCount + letterCount.get(String.valueOf((char) i)));
+		}
+	}
+	
 	public void printResults(String websiteURL)
 	{
 		System.out.println("[INFORMATION] File metadata:");
@@ -92,5 +140,13 @@ public class WebsiteFileReading
 		System.out.println("Lines: " + lines);
 		System.out.println("Words: " + words);
 		System.out.println("Characters: " + characters);
+	}
+	
+	public void printLetterCountMap()
+	{
+		// https://stackoverflow.com/a/32264826
+		letterCount.forEach((key, value) -> {
+			System.out.println("Key : " + key + " Value : " + value);
+		});
 	}
 }
