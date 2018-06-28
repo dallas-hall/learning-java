d60c2075e97910492d5e426127d8d2acd96a80b5
Dallas Hall <dallas.hall@example.com>
2018-06-28 20:43:27 +1000

    Showcases how the main method, no arg constructor, and the Application start method are launched in a JavaFX app.

---
+++ src/chapter14_javafx/LaunchSequence.java
@@ -0,0 +1,39 @@
+package chapter14_javafx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * <h1>LaunchSequence</h1>
+ * <p>
+ * This program
+ * </p>
+ * <p>
+ * tags:	<insert concept tags here for training code only>
+ * </p>
+ *
+ * @author blindcant
+ * @version 0.0.1 - 2018-06-28
+ */
+public class LaunchSequence extends Application
+{
+	//@@@ JAVAFX @@@
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		System.out.println("primaryStage.");
+	}
+	
+	//@@@ MAIN METHOD @@@
+	public static void main(String[] args)
+	{
+		System.out.println("main method");
+		Application.launch(args);
+	}
+	
+	//@@@ CONSTRUCTOR(S) @@@
+	public LaunchSequence()
+	{
+		System.out.println("Constructor.");
+	}
+}
