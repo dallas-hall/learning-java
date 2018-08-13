134112d6914a8f773a6506c02ea371718dcaffd6
Dallas Hall <dallas.hall@example.com>
2018-08-13 19:41:55 +1000

    Event handling demo. Buttons added to a Hbox with event handling via the EventHandler interface.

---
+++ src/chapter15_event_driven_programming/EventHandling.java
@@ -0,0 +1,79 @@
+package chapter15_event_driven_programming;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+/**
+ * <h1>EventHandling</h1>
+ * <p>
+ * This program shows how to do primitive event handling for buttons.
+ * </p>
+ * <p>
+ * tags:	HBox; Button; EventHandler<ActionEvent>;
+ * </p>
+ *
+ * @author blindcant
+ * @version 0.1.0 - 2018-08-13
+ */
+public class EventHandling extends Application
+{
+	//@@@ CLASS VARIABLES @@@
+	
+	
+	//@@@ INSTANCE VARIABLES @@@
+	public static final String PROGRAM_VERSION = "0.1.0";
+	
+	//@@@ MAIN METHOD @@@
+	public static void main(String[] args)
+	{
+		Application.launch(args);
+	}
+	
+	//@@@ JAVAFX @@@
+	@Override
+	public void start(Stage primaryStage)
+	{
+		HBox hBox = new HBox(10);
+		hBox.setAlignment(Pos.CENTER);
+		Button button_Ok = new Button("OK");
+		Button button_Cancel = new Button("Cancel");
+		
+		OkHandlerClass handler_buttonOk = new OkHandlerClass();
+		button_Ok.setOnAction(handler_buttonOk);
+		
+		CancelHandlerClass handler_buttonCancel = new CancelHandlerClass();
+		button_Cancel.setOnAction(handler_buttonCancel);
+		
+		hBox.getChildren().addAll(button_Ok, button_Cancel);
+		
+		Scene scene = new Scene(hBox);
+		primaryStage.setTitle("EventHandling");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	//@@@ INNER CLASS(ES) @@@
+	class OkHandlerClass implements EventHandler<ActionEvent>
+	{
+		@Override
+		public void handle(ActionEvent actionEvent)
+		{
+			System.out.println("OK button was clicked.");
+		}
+	}
+	
+	class CancelHandlerClass implements EventHandler<ActionEvent>
+	{
+		@Override
+		public void handle(ActionEvent actionEvent)
+		{
+			System.out.println("Cancel button was clicked.");
+		}
+	}
+}
