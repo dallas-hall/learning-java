b509ef6425e6ef69e63e064fe8f9783d16bde202
Dallas Hall <mr.dallas.hall@gmail.com>
2018-12-05 08:45:43 +1100

    Count down and count up recursion examples. The count up shows how stack frames can be accessed after the recursive call has finished.

--- src/Chapter05/CountdownRecursion.java
+++ src/Chapter05/CountdownRecursion.java
@@ -6,6 +6,8 @@ public class CountdownRecursion
 	{
 		CountdownRecursion countdownRecursion = new CountdownRecursion();
 		countdownRecursion.countdown(10);
+		System.out.println();
+		countdownRecursion.countup(3);
 	}
 
 	private void countdown(int i)
@@ -20,6 +22,28 @@ public class CountdownRecursion
 		}
 		else {
 			System.out.println("Blast off!");
+		}
+	}
+
+	private void countup(int i)
+	{
+		if (i < 0) {
+			System.out.println("Input must be greater than 0.");
+			return;
+		}
+		else if (i == 0) {
+			System.out.println("Blast off!");
+		}
+		else {
+			/*
+			The recursive calls here delay the println from executing.
+			Each time the call is made, a new frame is added to the stack, which holds the current value of i.
+			*/
+			countup(i - 1);
+			/*
+			Once the recursion is over, the println is executed before the stack frames are popped.
+			 */
+			System.out.println(i + "...");
 		}
 	}
 }
