fd76624c79d378dd65124e9d888f567c132f1cc7
Dallas Hall <mr.dallas.hall@gmail.com>
2018-12-06 19:45:33 +1100

    Test cases using Junit 5.

---
+++ test/Chapter05/FibonacciRecursionTest.java
@@ -0,0 +1,38 @@
+package Chapter05;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FibonacciRecursionTest
+{
+	FibonacciRecursion runtime;
+	
+	@BeforeEach
+	void setUp()
+	{
+		runtime = new FibonacciRecursion(5);
+	}
+	
+	@AfterEach
+	void tearDown()
+	{
+		runtime = null;
+	}
+	
+	@Test
+	void getLimit()
+	{
+		assertEquals(5, runtime.getLimit());
+	}
+	
+	@Test
+	void fibonacci()
+	{
+		assertEquals(1, runtime.fibonacci(1));
+		assertEquals(1, runtime.fibonacci(2));
+		assertEquals(2, runtime.fibonacci(3));
+	}
+}
