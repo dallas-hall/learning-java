f3aef1a3dd92c6f893a8cd9185fb800766fe2f89
Dallas Hall <mr.dallas.hall@gmail.com>
2018-12-11 08:14:33 +1100

    Printing multiplication table using while and for loops.

---
+++ src/Chapter07/MultiplicationTable.java
@@ -0,0 +1,55 @@
+package Chapter07;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MultiplicationTable
+{
+	//@@@ CLASS VARIABLES @@@
+	private static final Logger logger = Logger.getLogger(MultiplicationTable.class.getName());
+
+	//@@@ INSTANCE VARIABLES @@@
+	public static final String PROGRAM_VERSION = "0.1.0";
+	private int limit;
+
+	//@@@ MAIN METHOD @@@
+	public static void main(String[] args) throws InterruptedException
+	{
+		logger.log(Level.INFO, "Program starting.");
+		Thread.sleep(005);
+		MultiplicationTable runtime = new MultiplicationTable();
+
+		logger.log(Level.INFO, "Multiplication Table (while)");
+		Thread.sleep(005);
+		runtime.printMultiplicationTableWhile(2, 50);
+
+		logger.log(Level.INFO, "Multiplication Table (for)");
+		Thread.sleep(005);
+		runtime.printMultiplicationTableFor(2, 50);
+	}
+
+	public void printMultiplicationTableWhile(int n, int limit)
+	{
+		int i = 1;
+		while (i <= limit) {
+			System.out.printf("%4d", n * i);
+
+			if (i % 10 == 0) {
+				System.out.println();
+			}
+
+			i++;
+		}
+	}
+
+	public void printMultiplicationTableFor(int n, int limit)
+	{
+		for (int i = 1; i <= limit; i++) {
+			System.out.printf("%4d", n * i);
+
+			if (i % 10 == 0) {
+				System.out.println();
+			}
+		}
+	}
+}
