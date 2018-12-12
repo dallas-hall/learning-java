65526451ea91401aedbec0c5556622b03a9fb70b
Dallas Hall <dallas.hall@example.com>
2018-12-12 15:00:15 +1100

    Added some more comments for teh approximate square root implementation.

--- src/Chapter07/Exercises.java
+++ src/Chapter07/Exercises.java
@@ -14,9 +14,16 @@ public class Exercises
 
 		logger.log(Level.INFO, "Exercise 2 - Approximate Square Root");
 		Thread.sleep(005);
+		System.out.println(runtime.approximateSquareRoot(1));
+		System.out.println(runtime.approximateSquareRoot(4));
 		System.out.println(runtime.approximateSquareRoot(9));
 		System.out.println(runtime.approximateSquareRoot(16));
 		System.out.println(runtime.approximateSquareRoot(25));
+		System.out.println(runtime.approximateSquareRoot(36));
+		System.out.println(runtime.approximateSquareRoot(49));
+		System.out.println(runtime.approximateSquareRoot(64));
+		System.out.println(runtime.approximateSquareRoot(83));
+		System.out.println(runtime.approximateSquareRoot(100));
 	}
 
 	public double approximateSquareRoot(double startNumber)
@@ -26,12 +33,14 @@ public class Exercises
 		double guess2 = 0.0;
 
 		while (true) {
+			// We are using the absolute number of the difference so we can always compare a positive integer.
 			double difference = Math.abs(guess1 - guess2);
 
-			if(difference < 0.0001) {
+			// If we decrease this number there will be more iterations and a more accurate result returned
+			// E.g. 0.0001 and 9 returns 3.0000000000393214 but 0.00001 returns 3.0
+			if (difference < 0.0001) {
 				return guess1;
-			}
-			else {
+			} else {
 				// Improve the guesses with the following formula
 				guess2 = (guess1 + startNumber / guess1) / 2;
 				guess1 = (guess2 + startNumber / guess2) / 2;
