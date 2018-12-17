234a43190d5ded9c440e6a3948619c068cde861d
Dallas Hall <mr.dallas.hall@gmail.com>
2018-12-18 09:03:20 +1100

    Euler's number answer.

---
+++ src/Chapter07/MyExpAnswer.java
@@ -0,0 +1,42 @@
+package Chapter07;
+
+public class MyExpAnswer
+{
+	// https://github.com/AllenDowney/thinkjavasolutions5/blob/master/old_solutions/Exp.java
+	public static void main(String[] args)
+	{
+		System.out.println(myexp(2, 0));
+		System.out.println(myexp(2, 1));
+		System.out.println(myexp(2, 2));
+		System.out.println(myexp(2, 3));
+		System.out.println();
+
+		for (int i = 1; i < 10; i++) {
+			check(i);
+		}
+	}
+
+	public static double myexp(double x, int n)
+	{
+		int i = 1;
+		double total = 1.0;
+		double num = 1.0;
+		double den = 1.0;
+
+		while (i < n) {
+			num = num * x;
+			den = den * i;
+			total = total + num / den;
+			i = i + 1;
+		}
+		return total;
+	}
+
+	public static void check(double x)
+	{
+		System.out.print(x + "\t");
+		System.out.print(myexp(x, 20) + "\t");
+		System.out.println(Math.exp(x));
+
+	}
+}
