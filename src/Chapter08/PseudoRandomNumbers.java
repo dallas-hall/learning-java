cf7ca9bc3c22e40c111aba3b74d3bd4ab4454fab
Dallas Hall <mr.dallas.hall@gmail.com>
2018-12-20 08:42:13 +1100

    Pseudo random number generation.

--- src/Chapter08/PseudoRandomNumbers.java
+++ src/Chapter08/PseudoRandomNumbers.java
@@ -5,14 +5,31 @@ import java.util.Random;
 
 public class PseudoRandomNumbers
 {
+	private int[] numbers;
+
 	public static void main(String[] args)
 	{
+		PseudoRandomNumbers prn = new PseudoRandomNumbers();
+		prn.setNumbers(prn.createArray());
+		System.out.println(Arrays.toString(prn.getNumbers()));
+	}
+
+	public int[] createArray() {
 		Random prn = new Random();
 		int[] numbers = new int[prn.nextInt(32)];
 		for (int i = 0; i < numbers.length; i++) {
 			numbers[i] = prn.nextInt(100);
 		}
+		return numbers;
+	}
 
-		System.out.println(Arrays.toString(numbers));
+	public int[] getNumbers()
+	{
+		return numbers;
+	}
+
+	public void setNumbers(int[] numbers)
+	{
+		this.numbers = numbers;
 	}
 }
