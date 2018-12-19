fc7165cf8419e13c7be32486a9bb39011659575a
Dallas Hall <mr.dallas.hall@gmail.com>
2018-12-19 14:06:29 +1100

    Could not figure out guass.

--- src/Chapter07/Exercises.java
+++ src/Chapter07/Exercises.java
@@ -30,7 +30,7 @@ public class Exercises
 		Thread.sleep(005);
 		// Overflow at 21
 		for (int i = 0; i <= 21; i++) {
-			System.out.println(i + " factorial = "+ runtime.factorialIterative(i));
+			System.out.println(i + " factorial = " + runtime.factorialIterative(i));
 		}
 
 		logger.log(Level.INFO, "Exercise 5 - Infinite Series Expansion");
@@ -173,5 +173,51 @@ public class Exercises
 		System.out.printf("%-18s\t%.19g\n", "myExp2(" + x + ", " + rounds + ")", myExp2(x, rounds));
 		System.out.printf("%-18s\t%.19g\n", "Math.exp(" + x + ")", Math.exp(x));
 		System.out.println();
+	}
+
+	public double gauss(double x, int n)
+	{
+		// https://en.wikipedia.org/wiki/Gaussian_integral
+		// e is euler's number. e ^ (-x ^ 2) = 1 - x ^ 2 + x ^ 4 /2 - ... + x ^ i ^ i
+		double total = 0;
+		double result = 0;
+		int exponent = 2;
+		int divisor = 2;
+
+		for (int i = 0; i < n; i++) {
+			if (i == 0) {
+				total += 1;
+			} else if (i == 1) {
+				result = Math.pow(x, exponent);
+				total -= result;
+				exponent += 2;
+			} else if (i == n - 1) {
+				// the last step is meant to be (-1)^ i * x ^ (2 ^ i) / i!
+			} else if (i % 2 == 0) {
+				result = Math.pow(x, exponent) / divisor;
+				total += result;
+				divisor += 4;
+				exponent += 2;
+			} else {
+				result = Math.pow(x, exponent) / divisor;
+				total -= result;
+				divisor += 4;
+				exponent += 2;
+			}
+		}
+		return total;
+	}
+
+	// https://github.com/ApolloZhu/Think-Java-Exercises/blob/master/Chapter7/7.6.java
+	public double otherGauss(double x, int n)
+	{
+		double result = 1;
+		int q = 1;
+		double p = 1;
+		int pre = 1;
+		for (int i = 1; i < n; i++) {
+			result += (pre *= -1) * (p *= x * x) / (q *= i);
+		}
+		return result;
 	}
 }
