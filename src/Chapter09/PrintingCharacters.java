4d82687e31470f30bc083c7ca53fd05bcc01fea1
Dallas Hall <dallas.hall@example.com>
2018-12-21 14:17:51 +1100

    Converting strings to char[] and printing each element.

--- src/Chapter09/PrintingCharacters.java
+++ src/Chapter09/PrintingCharacters.java
@@ -6,7 +6,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class PrintingCharacters
-{	private static final Logger logger = Logger.getLogger(PrintingCharacters.class.getName());
+{
+	private static final Logger logger = Logger.getLogger(PrintingCharacters.class.getName());
 
 	public static void main(String[] args) throws InterruptedException
 	{
@@ -14,20 +15,26 @@ public class PrintingCharacters
 
 		logger.log(Level.INFO, "Printing lowercase English characters.");
 		Thread.sleep(005);
-		runtime.printCharacters('a', 'z');
+		runtime.printCharacterRange('a', 'z');
 
 		logger.log(Level.INFO, "Printing uppercase English characters.");
 		Thread.sleep(005);
-		runtime.printCharacters((char) 65, (char) 90);
+		runtime.printCharacterRange((char) 65, (char) 90);
 
 		logger.log(Level.INFO, "Printing Korean characters.");
 		Thread.sleep(005);
+		runtime.printCharacterRange('\u3131', '\u318E');
 
-		runtime.printCharacters('\u3131', '\u318E');
+		logger.log(Level.INFO, "Printing all characters from a string (for loop).");
+		Thread.sleep(005);
+		runtime.printAllStringCharactersForLoop("Hi, I am a string.");
 
+		logger.log(Level.INFO, "Printing all characters from a string (for each loop).");
+		Thread.sleep(005);
+		runtime.printAllStringCharactersForEachLoop("Hi, I am a string.");
 	}
 
-	public void printCharacters(char firstChar, char endChar)
+	public void printCharacterRange(char firstChar, char endChar)
 	{
 		int prints = 0;
 		for (char c = firstChar; c <= endChar; c++) {
@@ -38,5 +45,30 @@ public class PrintingCharacters
 			}
 		}
 		System.out.println();
+	}
+
+	public void printAllStringCharactersForLoop(String s)
+	{
+		System.out.print("{'" + s.charAt(0) + "'");
+		for (int i = 1; i < s.length(); i++) {
+			System.out.print(", '" + s.charAt(i) + "'");
+		}
+		System.out.println("}");
+	}
+
+	public void printAllStringCharactersForEachLoop(String s)
+	{
+		System.out.print("{");
+		int i = 0;
+		// Need to convert from String to array as for each loops can only iterate over collections
+		for(char c: s.toCharArray()) {
+			if (i == 0) {
+				System.out.print("'" + c + "'");
+			} else {
+				System.out.print(", '" + c + "'");
+			}
+			i++;
+		}
+		System.out.println("}");
 	}
 }
