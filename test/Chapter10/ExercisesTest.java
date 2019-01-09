809a4921bb4f30418d9ee4c9428f275238ab16a9
Dallas Hall <dallas.hall@example.com>
2019-01-09 12:51:39 +1100

    BigInteger power recursion unit test.

--- test/Chapter10/ExercisesTest.java
+++ test/Chapter10/ExercisesTest.java
@@ -8,11 +8,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ExercisesTest
 {
 	private static BigIntegerFactorial bigIntegerFactorial;
+	private static BigIntegerPowerRecursive bigIntegerPowerRecursive;
 	
 	@org.junit.jupiter.api.BeforeAll
 	static void initAll()
 	{
 		bigIntegerFactorial = new BigIntegerFactorial();
+		bigIntegerPowerRecursive = new BigIntegerPowerRecursive();
 
 	}
 	
@@ -47,5 +49,26 @@ public class ExercisesTest
 		checkAnswer = BigInteger.valueOf(120);
 		System.out.println(n + "! is " + checkAnswer);
 		assertEquals(checkAnswer, bigIntegerFactorial.factorialIterativeBigInteger(n));
+	}
+	
+	@org.junit.jupiter.api.Test
+	void powerRecursionInt()
+	{
+		int startNumber = 2;
+		for (int i = 0; i < 17; i++) {
+			System.out.printf("%s %s.\n", startNumber + " ^ " + i + " is", bigIntegerPowerRecursive.powerRecursionInt(startNumber, i)) ;
+			assertEquals(Math.pow(startNumber, i), bigIntegerPowerRecursive.powerRecursionInt(startNumber, i));
+		}
+	}
+	
+	@org.junit.jupiter.api.Test
+	void powerRecursiveBigInteger()
+	{
+		int startNumber = 2;
+		for (int i = 0; i < 17; i++) {
+			System.out.printf("%s %s.\n", startNumber + " ^ " + i + " is", bigIntegerPowerRecursive.powerRecursiveBigInteger(startNumber, i)) ;
+			// Need to cast the double to an int as BigInteger only works with int
+			assertEquals(BigInteger.valueOf((int) Math.pow(startNumber, i)), bigIntegerPowerRecursive.powerRecursiveBigInteger(startNumber, i));
+		}
 	}
 }
