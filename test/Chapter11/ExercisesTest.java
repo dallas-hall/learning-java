b18db673ea86725a1161b3c8da827212bad86248
Dallas Hall <dallas.hall@example.com>
2019-01-10 19:26:55 +1100

    Unit tests for the RationalNumber methods.

--- test/Chapter11/ExercisesTest.java
+++ test/Chapter11/ExercisesTest.java
@@ -71,6 +71,66 @@ public class ExercisesTest
 	}
 	
 	@org.junit.jupiter.api.Test
+	void createRationalNumber()
+	{
+		System.out.println("Checking rationalNumber constructors");
+		assertEquals(0, rationalNumber.getNumerator());
+		assertEquals(1, rationalNumber.getDenominator());
+		rationalNumber = new RationalNumber(1, 3);
+		assertEquals(1, rationalNumber.getNumerator());
+		assertEquals(3, rationalNumber.getDenominator());
+	}
+	
+	@org.junit.jupiter.api.Test
+	void rationalNumberToString()
+	{
+		System.out.println("Checking rationalNumber toString");
+		rationalNumber = new RationalNumber(1, 3);
+		assertEquals("1/3", rationalNumber.toString());
+	}
+	
+	@org.junit.jupiter.api.Test
+	void rationalNumberEquals()
+	{
+		System.out.println("Checking rationalNumber equals");
+		rationalNumber = new RationalNumber(3, 5);
+		assertEquals(true, rationalNumber.equals(new RationalNumber(3, 5)));
+		assertEquals(false, rationalNumber.equals(new RationalNumber(2, 5)));
+	}
+	
+	@org.junit.jupiter.api.Test
+	void rationalNumberFlip()
+	{
+		System.out.println("Checking rationalNumber flip");
+		rationalNumber = new RationalNumber(1, 3);
+		rationalNumber.flipRationalNumber();
+		assertEquals(3, rationalNumber.getNumerator());
+		assertEquals(1, rationalNumber.getDenominator());
+	}
+	
+	@org.junit.jupiter.api.Test
+	void rationalNumberNegate()
+	{
+		System.out.println("Checking rationalNumber negate");
+		rationalNumber = new RationalNumber(1, 3);
+		rationalNumber.negateRationalNumber();
+		assert(rationalNumber.getNumerator() == 1 || rationalNumber.getNumerator() == -1);
+		assert(rationalNumber.getDenominator() == 3 || rationalNumber.getDenominator() == -3);
+	}
+	
+	@org.junit.jupiter.api.Test
+	void rationalNumberToDouble()
+	{
+		System.out.println("Checking rationalNumber toDouble");
+		rationalNumber = new RationalNumber(1, 4);
+		assertEquals(0.25, rationalNumber.toDouble());
+		rationalNumber = new RationalNumber(1, 3);
+		assertEquals(0.3333333333333333, rationalNumber.toDouble());
+		rationalNumber = new RationalNumber(1, 2);
+		assertEquals(0.5, rationalNumber.toDouble());
+	}
+	
+	@org.junit.jupiter.api.Test
 	void gcdEuclidSubtractionRecursion()
 	{
 		System.out.println("Testing gcdEuclidSubtractionRecursion with\n" +
@@ -98,5 +158,29 @@ public class ExercisesTest
 		assertEquals(1, rationalNumber.gcdEuclidModuloRecursion(37, 600));
 		assertEquals(20, rationalNumber.gcdEuclidModuloRecursion(20, 100));
 		assertEquals(18913, rationalNumber.gcdEuclidModuloRecursion(624129, 2061517));
+	}
+	
+	@org.junit.jupiter.api.Test
+	void rationalNumberReduce()
+	{
+		System.out.println("Checking rationalNumber reduce");
+		rationalNumber = new RationalNumber(2, 4);
+		assertEquals(true, rationalNumber.reduce().equals(new RationalNumber(1, 2)));
+		assertEquals(false, rationalNumber.reduce().equals(new RationalNumber(1, 3)));
+		rationalNumber = new RationalNumber(6, 9);
+		assertEquals(true, rationalNumber.reduce().equals(new RationalNumber(2, 3)));
+		assertEquals(false, rationalNumber.reduce().equals(new RationalNumber(1, 3)));
+	}
+	
+	@org.junit.jupiter.api.Test
+	void rationalNumberAdd()
+	{
+		System.out.println("Checking rationalNumber addRationals");
+		rationalNumber = new RationalNumber(1, 3);
+		assertEquals(true, rationalNumber.addRationals(1, 6)
+			.equals(new RationalNumber(1, 2)));
+		rationalNumber = new RationalNumber(2, 4);
+		assertEquals(true, rationalNumber.addRationals(4, 6)
+										 .equals(new RationalNumber(7, 6)));
 	}
 }
